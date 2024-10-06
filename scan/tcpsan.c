b04a3de449c90b20cca15322dfad5153b0310421
qq1521575701 <136119411+qq1521575701@users.noreply.github.com>
2024-10-07 02:03:11 +0800

    Update tcpsan.c

--- scan/tcpsan.c
+++ scan/tcpsan.c
@@ -13,6 +13,7 @@ unsigned short checksum(unsigned short *b, int len) {
     unsigned int sum = 0;
     unsigned short answer = 0;
 
+    // 计算校验和
     for (sum = 0; len > 1; len -= 2) {
         sum += *p++;
     }
@@ -33,47 +34,54 @@ int create_socket(char *src_ip, int src_port, char *dst_ip) {
     struct tcphdr tcph;
     char packet[4096];
 
+    // 创建套接字
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sockfd < 0) {
-        printf("套接字创建失败 %s\n",dst_ip);
+        printf("套接字创建失败 %s\n", dst_ip);
         return -1; // 失败返回
     }
 
     // 填充 IP 头部
     memset(&iph, 0, sizeof(iph));
-    iph.ihl = 5;
-    iph.version = 4;
-    iph.tos = 0;
-    iph.tot_len = sizeof(iph) + sizeof(tcph) + strlen("GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n");
-    iph.id = htons(54321);
-    iph.frag_off = 0;
-    iph.ttl = 255;
-    iph.protocol = IPPROTO_TCP;
-    iph.check = 0;
-    iph.saddr = inet_addr(src_ip);
-    iph.daddr = inet_addr(dst_ip);
+    iph.ihl = 5; // IP 头部长度
+    iph.version = 4; // IPv4
+    iph.tos = 0; // 服务类型
+    iph.tot_len = sizeof(iph) + sizeof(tcph) + strlen("GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n");
+    iph.id = htons(12345); // 标识符
+    iph.frag_off = 0; // 分段偏移
+    iph.ttl = 255; // 生存时间
+    iph.protocol = IPPROTO_TCP; // 协议类型
+    iph.check = 0; // 校验和（初始为0）
+    iph.saddr = inet_addr(src_ip); // 源 IP 地址
+    iph.daddr = inet_addr(dst_ip); // 目的 IP 地址
 
     // 计算 IP 校验和
     iph.check = checksum((unsigned short *)&iph, sizeof(iph));
 
     // 填充 TCP 头部
     memset(&tcph, 0, sizeof(tcph));
-    tcph.source = htons(src_port);
-    tcph.dest = htons(80);
-    tcph.seq = htonl(rand() % (UINT32_MAX - 100000) + 100000);
-    tcph.ack_seq = 0;
-    tcph.doff = 5;
-    tcph.syn = 1;
-    tcph.window = htons(65535);
-    tcph.check = 0;
+    tcph.source = htons(src_port); // 源端口
+    tcph.dest = htons(80); // 目的端口
+    tcph.seq = htonl(rand() % (UINT32_MAX - 100000) + 100000); // 随机序列号
+    tcph.ack_seq = htonl(5000); // 确认序列号
+    tcph.doff = 5; // TCP 头部长度
+    tcph.fin = 0; // FIN 标志
+    tcph.syn = 0; // SYN 标志（初始化连接时设置）
+    tcph.rst = 0; // RST 标志
+    tcph.psh = 1; // PSH 标志
+    tcph.ack = 1; // ACK 标志（确认号有效）
+    tcph.urg = 1; // URG 标志（紧急指针有效）
+    tcph.urg_ptr = htons(10); // 紧急指针，假设指向第10个字节
+    tcph.window = htons(65535); // 窗口大小
+    tcph.check = 0; // 校验和（初始为0）
 
     // 填充伪头部
     struct pseudo_header {
-        u_int32_t source_address;
-        u_int32_t dest_address;
-        u_int8_t placeholder;
-        u_int8_t protocol;
-        u_int16_t tcp_length;
+        u_int32_t source_address; // 源地址
+        u_int32_t dest_address; // 目的地址
+        u_int8_t placeholder; // 保留字段
+        u_int8_t protocol; // 协议
+        u_int16_t tcp_length; // TCP 长度
     } psh;
 
     memset(&psh, 0, sizeof(psh));
@@ -81,37 +89,33 @@ int create_socket(char *src_ip, int src_port, char *dst_ip) {
     psh.dest_address = inet_addr(dst_ip);
     psh.placeholder = 0;
     psh.protocol = IPPROTO_TCP;
-    psh.tcp_length = htons(sizeof(tcph) + strlen("GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
+    psh.tcp_length = htons(sizeof(tcph) + strlen("GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
 
     // 计算 TCP 校验和
-    int psize = sizeof(psh) + sizeof(tcph) + strlen("GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n");
+    int psize = sizeof(psh) + sizeof(tcph) + strlen("GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n");
     char *pseudogram = malloc(psize);
     memcpy(pseudogram, (char *)&psh, sizeof(psh));
     memcpy(pseudogram + sizeof(psh), &tcph, sizeof(tcph));
-    memcpy(pseudogram + sizeof(psh) + sizeof(tcph), "GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n", strlen("GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
+    memcpy(pseudogram + sizeof(psh) + sizeof(tcph), "GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n", strlen("GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
+    
+    // 计算校验和并赋值给 TCP 头部
     tcph.check = checksum((unsigned short *)pseudogram, psize);
-    free(pseudogram);
+    free(pseudogram); // 释放内存
 
     // 创建数据包
     memset(packet, 0, 4096);
     memcpy(packet, &iph, sizeof(iph));
     memcpy(packet + sizeof(iph), &tcph, sizeof(tcph));
-    memcpy(packet + sizeof(iph) + sizeof(tcph), "GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n", strlen("GET / HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
+    memcpy(packet + sizeof(iph) + sizeof(tcph), "GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n", strlen("GET /index.php?id=1 and 1=1 HTTP/1.1\r\nHost: freedomhouse.org\r\n\r\n"));
 
-    // 目标地址
+    // 设置目标地址
     dest.sin_family = AF_INET;
     dest.sin_port = htons(80);
     dest.sin_addr.s_addr = iph.daddr;
 
     // 发送数据包
-    if (sendto(sockfd, packet, iph.tot_len, 0, (struct sockaddr *)&dest, sizeof(dest)) < 0) {
-        printf("发送失败 %s\n",dst_ip);
-    } 
-    else {
-        printf("发送成功 %s\n",dst_ip);
-    }
-
-    close(sockfd);
+    sendto(sockfd, packet, iph.tot_len, 0, (struct sockaddr *)&dest, sizeof(dest));
+    close(sockfd); // 关闭套接字
     return 0; // 成功返回
 }
 
